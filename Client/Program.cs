b52ea7e7b466548533958d64de7fcbe41ca67132
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2023-12-06 10:49:32 +0100

    Reinterop beginnings

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient<IErrorLogger, JsInterop>();
-builder.Services.AddTransient<>
+builder.Services.AddTransient<IRestInterop, RestInterop>();
 
 await builder.Build().RunAsync();
