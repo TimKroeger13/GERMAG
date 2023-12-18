2a7dc48cb847073d83fd548cb1ebd2c1b8e84be5
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2023-12-18 15:29:50 +0100

    Working Fecthdata download

--- Server/DataPulling/DataFetcher.cs
+++ Server/DataPulling/DataFetcher.cs
@@ -17,7 +17,7 @@ public class DataFetcher(IDatabaseUpdater databaseUpdater) : IDataFetcher
 
         // Example URL
         var currentURL = "https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_poly_entzugspot2400_100?service=wfs&version=2.0.0&request=GetFeature&typeNames=fis:s_poly_entzugspot2400_100";
-
+        
         var connectionFeedback = _databaseUpdater.updateDatabase(currentURL, "Entzugsleistung_100M_2400HP");
 
         Console.WriteLine(connectionFeedback);
