2fc34c484679e6f9da80fee15719e5033bcf14fe
Amadeus Lehmann <no@mail.com>
2024-02-01 16:31:33 +0100

    lowercase for docker image names

--- Confluence/Install/buildGERMAG.sh
+++ Confluence/Install/buildGERMAG.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo docker build -f Dockerfile --no-cache -t GERMAG:live .
+sudo docker build -f Dockerfile --no-cache -t germag:live .
 read -r -p "Database Password: " dbPassword 
 sudo docker run \
   -d -p 443:443 \
-  --name GERMAG-live \
+  --name germag-live \
   --env DATABASE_CONNECTION="Host=172.19.0.50:5432;Database=gasag;Username=postgres;Password=$dbPassword" \
   --restart always \
   -v /home/certs:/root/certs \
   --ip 172.19.0.51 \
   --network db-network \
-GERMAG:live
+germag:live
