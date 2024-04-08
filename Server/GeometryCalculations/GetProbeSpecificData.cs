291c10fa38f1638fb607857678066288aeed77c7
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2024-04-08 15:12:03 +0200

    Added calculation of geothermic values of probes

--- Server/GeometryCalculations/GetProbeSpecificData.cs
+++ Server/GeometryCalculations/GetProbeSpecificData.cs
@@ -17,7 +17,7 @@ public class GetProbeSpecificData(DataContext context, IParameterDeserialator pa
     {
         foreach (var SingleProbePoint in probePoints)
         {
-            if (SingleProbePoint == null || SingleProbePoint.Geometry == null)
+            if (SingleProbePoint == null || SingleProbePoint.Geometry == null || SingleProbePoint.Properties == null)
             {
                 continue;
             }
@@ -45,28 +45,88 @@ public class GetProbeSpecificData(DataContext context, IParameterDeserialator pa
                 }).ToList();
 
             var UnserilizedDepthRestrictions = intersectingResult.FirstOrDefault(element => element.Type == TypeOfData.depth_restrictions);
-            var DeserializedDepthRestrictions = parameterDeserialator.DeserializeParameters(UnserilizedDepthRestrictions?.Parameter ?? "");
-            var MaxDepth = DeserializedDepthRestrictions.VALUE;
 
-            if (MaxDepth == null)
+            double? MaxDepth;
+
+            if (UnserilizedDepthRestrictions == null)
             {
                 MaxDepth = 100; //Berlin spesific
+            }
+            else
+            {
+                var DeserializedDepthRestrictions = parameterDeserialator.DeserializeParameters(UnserilizedDepthRestrictions?.Parameter ?? "");
+                MaxDepth = DeserializedDepthRestrictions.VALUE;
+
             }
 
             //Poetential = 100,80,60,40
             List<int> PotentialDepth = new List<int> { 100, 80, 60, 40, 0}; //Berlin spesific
 
-            int nearestLowetDepth = PotentialDepth.FirstOrDefault(value => value <= MaxDepth);
+            int GeoPotenDepth = PotentialDepth.FirstOrDefault(value => value <= MaxDepth);
+
+            double? GeoPoten = null;
+
+            if (GeoPotenDepth >= 100)
+            {
+                GeoPoten = GetPotential(intersectingResult, TypeOfData.geo_poten_100m_with_2400ha, "La_100txt");
+            }
+            else if(GeoPotenDepth >= 80)
+            {
+                GeoPoten = GetPotential(intersectingResult, TypeOfData.geo_poten_100m_with_2400ha, "La_80txt");
+            }
+            else if (GeoPotenDepth >= 60)
+            {
+                GeoPoten = GetPotential(intersectingResult, TypeOfData.geo_poten_100m_with_2400ha, "La_60txt");
+            }
+            else if (GeoPotenDepth >= 40)
+            {
+                GeoPoten = GetPotential(intersectingResult, TypeOfData.geo_poten_100m_with_2400ha, "La_40txt");
+            }
+
+            SingleProbePoint.Properties.MaxDepth = MaxDepth;
+            SingleProbePoint.Properties.GeoPotenDepth = GeoPotenDepth;
+            SingleProbePoint.Properties.GeoPoten = GeoPoten;
 
 
-            var GeoPoten2400 = intersectingResult.FirstOrDefault(element => element.Type == TypeOfData.geo_poten_100m_with_2400ha);
-
-
-            var b = 3;
         }
 
         var a = probePoints.ToList();
 
         return probePoints;
+    }
+
+    private double? GetPotential (List<GeometryElementParameter> intersectingResult, TypeOfData typeOfData, string ParameterKeyValue)
+    {
+        GeometryElementParameter? UnserilizedGeoPoten = intersectingResult.FirstOrDefault(element => element.Type == typeOfData);
+        Shared.Properties DeserializedGeoPoten = parameterDeserialator.DeserializeParameters(UnserilizedGeoPoten?.Parameter ?? "");
+        double? GeoPoten = ParseStringToValue(DeserializedGeoPoten.La_100txt ?? "");
+
+        return GeoPoten;
+    }
+
+
+    private double? ParseStringToValue(string valueRange)
+    {
+        List<double> numbers = new List<double>();
+
+        string[] parts = valueRange.Split('-', '>');
+
+        foreach (string part in parts)
+        {
+            if (double.TryParse(part.Trim(), out double number))
+            {
+                numbers.Add(number);
+            }
+        }
+
+        if (numbers.Count == 0)
+        {
+            throw new Exception("ParseStringToValue: Numbers could not get paresed");
+        }
+
+        double minValue = numbers.Min();
+        double maxValue = numbers.Max();
+
+        return (minValue + maxValue) / 2.0;
     }
 }
--- Shared/PointProperties/ProbePoints.cs
+++ Shared/PointProperties/ProbePoints.cs
@@ -16,7 +16,7 @@ public class ProbePoint
 
 public class Properties
 {
-    public double? GeoPoten2400 { get; set; }
+    public double? GeoPoten { get; set; }
     public double? MaxDepth { get; set; }
     public double? GeoPotenDepth { get; set; }
 }
