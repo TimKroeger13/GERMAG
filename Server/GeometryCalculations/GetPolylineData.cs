dd2e1e46a2bca55de21bda2b070f842c206a646c
TimKroeger13 <t.kroeger13@googlemail.com>
2024-04-17 16:08:06 +0200

    Fixed Bugs in the Line Calculation

--- Server/GeometryCalculations/GetPolylineData.cs
+++ Server/GeometryCalculations/GetPolylineData.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
+using NetTopologySuite.Operation.Distance;
+using Microsoft.EntityFrameworkCore;
 
 namespace GERMAG.Server.GeometryCalculations;
 
@@ -18,14 +20,13 @@ public class GetPolylineData(DataContext context, IParameterDeserialator paramet
 {
     public async Task<double?> GetNearestPolylineData(LandParcel landParcelElement)
     {
-
-        var polyLineId = context.GeothermalParameter.Where(gp => gp.Geometry_Type == Geometry_Type.polyline)
+        var polyLineId = await context.GeothermalParameter.Where(gp => gp.Geometry_Type == Geometry_Type.polyline)
             .Select(gp => new
             {
                 gp.Type,
                 gp.Id
 
-            }).ToList();
+            }).ToListAsync();
 
         //ExpectedGroundWaterHight Case
 
@@ -33,19 +34,28 @@ public class GetPolylineData(DataContext context, IParameterDeserialator paramet
 
         if(ExpectedGroundWaterHight_info?.Id == null || landParcelElement.Geometry == null) { throw new Exception("Geometry for Line data Not Found"); }
 
-        var ExpectedGroundWaterHight_data = context.GeoData.Where(gd => gd.ParameterKey == ExpectedGroundWaterHight_info.Id && gd.Parameter != "")
+        var ExpectedGroundWaterHight_data = await context.GeoData
+            .Where(gd => gd.ParameterKey == ExpectedGroundWaterHight_info.Id && gd.Parameter != null)
             .Select(gd => new
             {
                 gd.Parameter,
                 Distance = gd.Geom!.Distance(landParcelElement.Geometry.Centroid)
-            }).OrderBy(gd => gd.Distance).FirstOrDefault(); //tolist
+            })
+            .OrderBy(gd => gd.Distance).Take(100)
+            .ToListAsync();
 
-        var DeserializedParameter = await Task.Run(() => parameterDeserialator.DeserializeParameters(ExpectedGroundWaterHight_data?.Parameter ?? ""));
+        var Zehgw_m_tx_Element = ExpectedGroundWaterHight_data.Select(expec => new
+        {
+            parameter = parameterDeserialator.DeserializeParameters(expec.Parameter!).Zehgw_m_tx,
+            Distance = expec.Distance
+        }).Where(x => x.parameter != "").FirstOrDefault();
 
-        if(DeserializedParameter.Zehgw_m_tx == "" || DeserializedParameter.Zehgw_m_tx == null) { return null; }
 
-        var zehgw = Convert.ToDouble(Regex.Replace(DeserializedParameter.Zehgw_m_tx, ",", "."), CultureInfo.InvariantCulture);
+        if(Zehgw_m_tx_Element  == null || Zehgw_m_tx_Element.parameter == "" || Zehgw_m_tx_Element.parameter == null) { return null; }
+
+        var zehgw = Convert.ToDouble(Regex.Replace(Zehgw_m_tx_Element.parameter, ",", "."), CultureInfo.InvariantCulture);
 
         return zehgw;
     }
 }
+//await Task.Run(() => parameterDeserialator.DeserializeParameters(UnserilizedDepthRestrictions?.Parameter ?? ""));
