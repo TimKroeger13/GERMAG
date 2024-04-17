8145ab3d2b69170db8ca12f43075fcbafef66751
TimKroeger13 <t.kroeger13@googlemail.com>
2024-04-17 13:35:30 +0200

    New Bug in LineSupportFound

--- Server/GeometryCalculations/GetPolylineData.cs
+++ Server/GeometryCalculations/GetPolylineData.cs
@@ -2,19 +2,21 @@
 using GERMAG.Server.ReportCreation;
 using GERMAG.Shared;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace GERMAG.Server.GeometryCalculations;
 
 public interface IGetPolylineData
 {
-    Task<double> GetNearestPolylineData(LandParcel landParcelElement);
+    Task<double?> GetNearestPolylineData(LandParcel landParcelElement);
 }
 
 public class GetPolylineData(DataContext context, IParameterDeserialator parameterDeserialator) : IGetPolylineData
 {
-    public async Task<double> GetNearestPolylineData(LandParcel landParcelElement)
+    public async Task<double?> GetNearestPolylineData(LandParcel landParcelElement)
     {
 
         var polyLineId = context.GeothermalParameter.Where(gp => gp.Geometry_Type == Geometry_Type.polyline)
@@ -31,16 +33,18 @@ public class GetPolylineData(DataContext context, IParameterDeserialator paramet
 
         if(ExpectedGroundWaterHight_info?.Id == null || landParcelElement.Geometry == null) { throw new Exception("Geometry for Line data Not Found"); }
 
-        var ExpectedGroundWaterHight_data = context.GeoData.Where(gd => gd.ParameterKey == ExpectedGroundWaterHight_info.Id)
+        var ExpectedGroundWaterHight_data = context.GeoData.Where(gd => gd.ParameterKey == ExpectedGroundWaterHight_info.Id && gd.Parameter != "")
             .Select(gd => new
             {
                 gd.Parameter,
                 Distance = gd.Geom!.Distance(landParcelElement.Geometry.Centroid)
-            }).OrderBy(gd => gd.Distance).FirstOrDefault();
+            }).OrderBy(gd => gd.Distance).FirstOrDefault(); //tolist
 
         var DeserializedParameter = await Task.Run(() => parameterDeserialator.DeserializeParameters(ExpectedGroundWaterHight_data?.Parameter ?? ""));
 
-        var zehgw = Convert.ToDouble(Regex.Replace(DeserializedParameter.Zehgw_m_tx ?? "", ",", "."), CultureInfo.InvariantCulture);
+        if(DeserializedParameter.Zehgw_m_tx == "" || DeserializedParameter.Zehgw_m_tx == null) { return null; }
+
+        var zehgw = Convert.ToDouble(Regex.Replace(DeserializedParameter.Zehgw_m_tx, ",", "."), CultureInfo.InvariantCulture);
 
         return zehgw;
     }
