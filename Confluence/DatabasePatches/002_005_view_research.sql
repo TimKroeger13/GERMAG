5e56d2a69e62bfa995e3ec1d3fdc5358463b37fc
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2024-04-23 18:00:08 +0200

    Possible Calcualtion of ax_select
    Maybe run in over night

--- Confluence/DatabasePatches/002_005_view_research.sql
+++ Confluence/DatabasePatches/002_005_view_research.sql
@@ -73,3 +73,43 @@ SELECT ST_Buffer(geom,4) FROM ax_tree
 
 SELECT ST_Buffer(geom,6) FROM ax_buildings
 --20
+
+
+--Killer statement All intersections
+CREATE TABLE ax_selected AS
+SELECT ST_UNION(ST_Intersection(usable_area.geom, parcel_area.geom)) AS geom
+FROM 
+(
+    SELECT ST_UNION(geom) AS geom 
+    FROM geo_data
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'area_usage'
+	)
+	AND parameter ->> 'Bezeich' = 'AX_Wohnbauflaeche'
+) AS usable_area,
+(
+    SELECT geom AS geom 
+    FROM geo_data
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'land_parcels'
+	)
+) AS parcel_area
+
+
+-- possible network distance
+SELECT ST_Length(
+    ST_LineSubstring(
+        line.geom,
+        ST_LineLocatePoint(line.geom, source_point.geom),
+        ST_LineLocatePoint(line.geom, destination_point.geom)
+    )
+) AS network_distance
+FROM lines AS line, points AS source_point, points AS destination_point
+WHERE source_point.id = <source_point_id>
+AND destination_point.id = <destination_point_id>;
