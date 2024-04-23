cc99eeba4114a828c783aebba549d607450471b5
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2024-04-23 10:55:51 +0200

    Flexibale research query

--- Confluence/DatabasePatches/002_005_view_research.sql
+++ Confluence/DatabasePatches/002_005_view_research.sql
@@ -1,11 +1,23 @@
 SELECT ST_Intersection(usable_area.geom, parcel_area.geom)
-FROM (
+FROM 
+(
     SELECT ST_Union(geom) AS geom 
     FROM geo_data
-    WHERE parameter_key = 34 AND parameter ->> 'Bezeich' = 'AX_Wohnbauflaeche'
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'area_usage'
+	)
+	AND parameter ->> 'Bezeich' = 'AX_Wohnbauflaeche'
 ) AS usable_area,
 (
     SELECT ST_Union(geom) AS geom 
     FROM geo_data
-    WHERE parameter_key = 1
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'land_parcels'
+	)
 ) AS parcel_area
