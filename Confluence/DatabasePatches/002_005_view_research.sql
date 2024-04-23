c1a825cc66bda9f957ec5c92bb134851d23d95b0
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2024-04-23 16:18:16 +0200

    Update 002_005_view_research.sql

--- Confluence/DatabasePatches/002_005_view_research.sql
+++ Confluence/DatabasePatches/002_005_view_research.sql
@@ -24,9 +24,39 @@ FROM
 ) AS parcel_area
 
 
+--Table creation
+
+--- ax_selected
+CREATE TABLE ax_selected AS
+SELECT ST_Intersection(usable_area.geom, parcel_area.geom) AS geom
+FROM 
+(
+    SELECT ST_Union(geom) AS geom 
+    FROM geo_data
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'area_usage'
+	)
+	AND parameter ->> 'Bezeich' = 'AX_Wohnbauflaeche'
+) AS usable_area,
+(
+    SELECT ST_Union(geom) AS geom 
+    FROM geo_data
+    WHERE parameter_key = 
+	(
+		SELECT id
+		FROM geothermal_parameter
+		WHERE typeofdata = 'land_parcels'
+	)
+) AS parcel_area
+
+--- ax_tree
+
 --Trees
 CREATE TABLE ax_tree AS
-SELECT ST_Union(geom) AS geom FROM geo_data
+SELECT geom AS geom FROM geo_data
 WHERE parameter_key = 36 OR parameter_key = 37
 
 
@@ -37,5 +67,9 @@ WHERE parameter_key = 32
 
 
 --AX_buffertrees
-CREATE TABLE ax_buffer_tree AS
+--CREATE TABLE ax_buffer_tree AS
 SELECT ST_Buffer(geom,4) FROM ax_tree
+--12 sec
+
+SELECT ST_Buffer(geom,6) FROM ax_buildings
+--20
