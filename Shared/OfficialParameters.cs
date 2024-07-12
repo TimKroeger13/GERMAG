b55aa0474747d11bb4ef9f51da14587de7e16615
TimKroeger13 <t.kroeger13@googlemail.com>
2024-07-12 09:08:13 +0200

    New Distances

--- Shared/OfficialParameters.cs
+++ Shared/OfficialParameters.cs
@@ -9,7 +9,7 @@ namespace GERMAG.Shared;
 
 public static class OfficalParameters
 {
-    public static double ProbeDiameter { get; } = 0.2; // meter (radius)
+    public static double ProbeDiameter { get; } = 0; // meter (radius)
     public static double TreeDistance { get; } = 4; // meter (radius)
     public static double LandParcelDistance { get; } = 3; // meter 0 für die AUswertung
     public static double BuildingDistance { get; } = 2; // meter
