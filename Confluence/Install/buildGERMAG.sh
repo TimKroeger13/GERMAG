c4c9a5a27660437ed7709653fe3bb48494f6824c
TimKroeger13 <t.kroeger13@googlemail.com>
2024-08-07 19:40:57 +0200

    Update buildGERMAG.sh

--- Confluence/Install/buildGERMAG.sh
+++ Confluence/Install/buildGERMAG.sh
@@ -3,7 +3,7 @@ sudo docker build -f Dockerfile --no-cache -t germag:live .
 read -r -p "Database Password: " dbPassword 
 sudo docker rm -f germag-live
 sudo docker run \
-  -d -p 9987:9987 \
+  -d -p 8443:443 \
   --name germag-live \
   --env DATABASE_CONNECTION="Host=172.19.0.50:5432;Database=gasag;Username=postgres;Password=$dbPassword" \
   --restart always \
